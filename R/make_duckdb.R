c7bd40553650107295b250990997ab1292a589e7
Melissa Albino Hegeman <melissa.hegeman@gmail.com>
2024-11-19 00:18:13 -0500

    Cleaned up code
    Removed extraneous tests and data. Created single script to load spatial data and a separate one to read it.

--- R/make_duckdb.R
+++ R/make_duckdb.R
@@ -1,65 +1,46 @@
-library(duckdb)
-library(duckdbfs)
-library(DBI)
+# Script to get point and polygon data into tables in duckdb
+
+# Load required libraries
 library(sf)
-library(tidyverse)
+library(duckdb)
+
+# Read the shapefile
+# Replace the file path with your actual shapefile path
+polygon_data <- st_read("data/NYS_Civil_Boundaries/Counties_Shoreline.shp")
+
+# Create a DuckDB connection, a new db will be created if it doesn't exist already
+con <- dbConnect(duckdb(), dbdir = "spatial_db.duckdb")
+
+# Make sure the spatial extension is installed and loaded
+dbExecute(con,
+          "INSTALL spatial;
+          LOAD spatial;")
+
+# Assuming spatial_data is your sf object
+# Convert geometry to WKT
+polygon_df <- data.frame(polygon_data)
+polygon_df$geometry <- st_as_text(polygon_data$geometry)
+
+# Create a table in DuckDB and load the data
+dbWriteTable(con, "counties_polygon", polygon_df)
+
+# Verify the data was loaded correctly
+result <- dbGetQuery(con, "SELECT * FROM counties_polygon LIMIT 5")
 
 
-con <- dbConnect(duckdb::duckdb(), dbdir = "test.duckdb")
-load_spatial(con)
+# Add point data from a parquet file
+points_file <- here::here("data", "campsite_amenities.parquet")
 
-dbListTables(con)
-
-# dbExecute(con, "DROP TABLE cleaned_points;")
-
-points_file <- here::here("data", "points.parquet")
-polygon_file <- here::here("data", "NYS_Civil_Boundaries", "Counties_Shoreline.shp")
-
-qry2 <- glue::glue("CREATE TABLE points AS
-            SELECT  pointid,
-                    rowid,
+# Create table in DuckDB and load data
+points_qry <- glue::glue("CREATE TABLE points AS
+            SELECT  OBJECTID,
+                    FACILITY,
+                    ASSET,
+                    PUBLIC_USE,
                     ST_Point(longitude, latitude) AS geom
-            FROM '{f}';")
+            FROM '{points_file}';")
+dbExecute(con, points_qry)
 
-dbExecute(con, qry2)
-
-
-
-#Add polygons to duckbd
-
-
-polygon_file <- "data/NYS_Civil_Boundaries/Counties_Shoreline.shp"
-polygon <- st_read(polygon_file) |>
-  mutate(rowid = row_number())
-
-
-
-# df_with_geom <- st_write(polygon, dsn = NULL, driver = "GeoJSON", quiet = TRUE)
-polygon$geometry <- st_as_text(st_geometry(polygon))
-
-polygon_qry <- glue::glue("CREATE TABLE counties AS SELECT  rowid, NAME, ST_GeomFromText(geometry) AS geom
-            FROM polygon;")
-
-
-dbExecute(con, polygon_qry)
-
-
-qry3 <- "SELECT *
-         FROM cleaned_points"
-
-test3 <- dbGetQuery(con, qry3) |>
-  head(20) |>
-  to_sf(crs = 4326, conn = con)
-glimpse(test3)
-
-# Now try your query with ST_AsWKB
-test4 <- dbGetQuery(con, "SELECT pointid, sub_area, ST_AsText(geom) AS test FROM points") |>
-  head(20) %>%
-  st_as_sf(wkt = "test", crs = 4326)
-
-
-library(ggplot2)
-ggplot() +
-  geom_sf(data = test4) +
-  theme_minimal()
+# Don't forget to close the connection when you're done
+dbDisconnect(con)
 
