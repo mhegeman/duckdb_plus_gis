12fe0976fd49cb89480df28d8ac3838c284bee66
Melissa Albino Hegeman <melissa.hegeman@gmail.com>
2024-11-24 11:24:54 -0500

    Simplify code

--- R/make_duckdb.R
+++ R/make_duckdb.R
@@ -4,10 +4,6 @@
 library(sf)
 library(duckdb)
 
-# Read the shapefile
-# Replace the file path with your actual shapefile path
-polygon_data <- st_read("data/NYS_Civil_Boundaries/Counties_Shoreline.shp")
-
 # Create a DuckDB connection, a new db will be created if it doesn't exist already
 con <- dbConnect(duckdb(), dbdir = "spatial_db.duckdb")
 
@@ -15,6 +11,10 @@ con <- dbConnect(duckdb(), dbdir = "spatial_db.duckdb")
 dbExecute(con,
           "INSTALL spatial;
           LOAD spatial;")
+
+# Read the shapefile
+# Replace the file path with your actual shapefile path
+polygon_data <- st_read("data/NYS_Civil_Boundaries/Counties_Shoreline.shp")
 
 # Assuming spatial_data is your sf object
 # Convert geometry to WKT
